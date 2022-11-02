02bae02d63fc58499a6a99ccb148af8f299bb32e
ykadirberk <ykadirberk2@gmail.com>
2022-11-02 18:11:05 +0300

    Proper main file

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Ilk denemelerimiz
-<<<<<<< HEAD
 		//ikinci deneme
-=======
 		//Guncelle
->>>>>>> 2657f869d3ae4f2e0a7f2d7e7900fb8ddbbc4b17
 
 	}
 
