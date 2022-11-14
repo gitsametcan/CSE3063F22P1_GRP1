659427ec4e2ba4e86ecb293a1f63cff6bdcc22df
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2022-11-14 18:30:37 +0300

    Progress on 2 JSONjava files

--- src/data/json/AdvisorJSON.java
+++ src/data/json/AdvisorJSON.java
@@ -3,15 +3,25 @@ package data.json;
 import java.util.ArrayList;
 import java.util.List;
 
+import Debt_LRA_Transcript.LectureRegistrationApplication;
+import person.Student;
+
 public class AdvisorJSON extends InstructorJSON {
+	
+	
+	
 	private List<String> listOfStudentIDs;
 	
-	public AdvisorJSON() {
+	public AdvisorJSON(String firstName, String lastName) {
+		super(firstName,lastName);
 		listOfStudentIDs = new ArrayList<String>();
+		this.firstName = firstName;
+		this.lastName = lastName;
 		
 	}
 	
 	public void addStudent(String id) {
+		listOfStudentIDs.add(id);
 		
 	}
 }
--- src/data/json/InstructorJSON.java
+++ src/data/json/InstructorJSON.java
@@ -2,4 +2,10 @@ package data.json;
 
 public class InstructorJSON extends PersonJSON{
 
+	public InstructorJSON(String firstName, String lastName) {
+		super(firstName, lastName);
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
 }
