175d2d25c9b7ce086a43a500fe5aec3ec9ff4aec
ykadirberk <ykadirberk2@gmail.com>
2022-11-14 18:02:39 +0300

    Progression on PersonJSON and StudentJSON

--- src/data/json/PersonJSON.java
+++ src/data/json/PersonJSON.java
@@ -1,5 +1,11 @@
 package data.json;
 
 public class PersonJSON {
-
+	protected String firstName;
+	protected String lastName;
+	
+	public PersonJSON(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 }
--- src/data/json/StudentJSON.java
+++ src/data/json/StudentJSON.java
@@ -1,5 +1,27 @@
 package data.json;
 
-public class StudentJSON extends PersonJSON{
+import java.util.HashMap;
+import java.util.Map;
 
+public class StudentJSON extends PersonJSON {
+	
+	private String advisorID;
+	private String studentID;
+	// CourseID/SessionID
+	private Map<String, String> sessions;
+	
+	public StudentJSON(String firstName, String lastName) {
+		super(firstName, lastName);
+		sessions = new HashMap<String, String>();
+	}
+	
+	public void setAdvisorID(String id) {
+		this.advisorID = id;
+	}
+	
+	public void setStudentID(String id) {
+		this.studentID = id;
+	}
+	
+	
 }
