363176d8330aac6f0ececdfabf97d9620f6c0c95
ykadirberk <ykadirberk2@gmail.com>
2022-11-16 00:49:25 +0300

    Solved a few problems, Changed DCD according to fixes

--- src/IDs/LectureID.java
+++ src/IDs/LectureID.java
@@ -3,39 +3,22 @@ package IDs;
 //Samet CAN 150120528
 public class LectureID implements UniqueID{
 
-	private String departmentCode;
-	private int yearCode;
 	private String lectureCode;
 
-	public LectureID(String departmentCode, int yearCode, String lectureCode) {
+	public LectureID(String lectureCode) {
 		super();
-		this.departmentCode = departmentCode;
-		this.yearCode = yearCode;
 		this.lectureCode = lectureCode;
 	}
 	//Another set method for ID
 	public void setID(String string) {
-		try {
-			Integer.parseInt(string.substring(3));
-		} catch (NumberFormatException e) {
+		this.lectureCode = string;
 
-		} finally {
-			this.departmentCode = string.substring(0, 3);
-			this.yearCode = Integer.parseInt(string.substring(3, 6));
-			this.lectureCode = string.substring(6);
-		}
-
-	}
-	//Set and get methods for ID
-	public void setID(String DepartmentCode, int YearCode, String LectureCode) {
-		this.departmentCode = DepartmentCode;
-		this.yearCode = YearCode;
-		this.lectureCode = LectureCode;
 	}
 
 	public String getID() {
-		return digitFixer(Integer.parseInt(departmentCode)) + digitFixer(yearCode) + lectureCode;
+		return lectureCode;
 	}
+
 	//A method for giving fixed amount of numbers according to input number
 	public String digitFixer(int integer) {
 		String tempOrder = "" + integer;
