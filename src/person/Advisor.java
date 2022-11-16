15010685f49627d4debbb78f471e29fca559f3c4
KaanCamci <kncmc2001@gmail.com>
2022-11-16 14:55:15 +0300

    approveLectures

--- src/person/Advisor.java
+++ src/person/Advisor.java
@@ -3,11 +3,14 @@ package person;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
 import Debt_LRA_Transcript.LectureRegistrationApplication;
+import Enums.ApprovalState;
 import Enums.InstructorType;
 import IDs.InstructorID;
 import lecture.Lecture;
+import lecture.LectureSession;
 
 //Kaan Camci 150119063
 public class Advisor extends Instructor {
@@ -24,7 +27,12 @@ public class Advisor extends Instructor {
 	}
 
 	public void approveApplication(LectureRegistrationApplication lectureRegistirationApplication) {
-		
+		for(LectureRegistrationApplication lra: listOfApplications) {
+			Map<LectureSession, ApprovalState> lsa = lra.getSessionsSentForApproval();
+			for(LectureSession ls: lsa.keySet()) {
+				lsa.put(ls, ApprovalState.Approved);
+			}
+		}
 	}
 
 	public Advisor(String firstName, String lastName, InstructorID id, List<Lecture> listOfLectures, Calendar dateOfEntry, List<Student> listOfStudents,
