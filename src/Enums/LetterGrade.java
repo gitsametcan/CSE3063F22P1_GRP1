7caa663b239aab05230d1df813f0549ff65b47e8
ykadirberk <ykadirberk2@gmail.com>
2022-11-16 18:32:48 +0300

    Added None(0) to LetterGrade

--- src/Enums/LetterGrade.java
+++ src/Enums/LetterGrade.java
@@ -2,7 +2,7 @@ package Enums;
 
 //Samet CAN 150120528
 public enum LetterGrade {
-	AA(4.00), BA(3.50), BB(3.00), CB(2.50), CC(2.00), DC(1.50), DD(1.00), FD(0.50), FF(0.00);
+	AA(4.00), BA(3.50), BB(3.00), CB(2.50), CC(2.00), DC(1.50), DD(1.00), FD(0.50), FF(0.00), None(0.00);
 
 	private final double grade;
 
