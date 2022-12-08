37776b1ceed4adcdb08ef7755affef52259e5c88
maknoli <serdar01alsan@outlook.com>
2022-12-08 18:43:26 +0300

    Update At Person Unit Test

--- src/test/unit/AdvisorTest.java
+++ src/test/unit/AdvisorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.Test;
@@ -17,20 +18,34 @@ import person.Student;
 
 public class AdvisorTest {
 	@Test
-	public void test_advisor_default_conscructor() {
+	public void test_advisor_list_of_student() {
 		
 		InstructorID testInstructorID = new InstructorID(1,2);
 		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
-		Calendar testDateOfEntry = null;
+		Calendar testDateOfEntry = new GregorianCalendar(2013,10,28);
 		List<Student> testListOfStudents = new ArrayList<Student>();
 		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
 		InstructorType testInstructorType = InstructorType.Assistant;
 		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
 	
 		assertEquals(testListOfStudents, testAdvisor.getListOfStudents());
+	}	
+		
+	@Test
+	public void test_advisor_list_of_application() {
+		
+		InstructorID testInstructorID = new InstructorID(1,2);
+		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
+		Calendar testDateOfEntry = new GregorianCalendar(2013,10,28);
+		List<Student> testListOfStudents = new ArrayList<Student>();
+		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
+		InstructorType testInstructorType = InstructorType.Assistant;
+		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
+	
 		assertEquals(testListOfApplications, testAdvisor.getListOfApplications());
+	}
 		
 	
 	
-	}
+	
 }
--- src/test/unit/StudentTest.java
+++ src/test/unit/StudentTest.java
@@ -4,15 +4,19 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.Test;
 
 import Debt_LRA_Transcript.LectureRegistrationApplication;
+import Debt_LRA_Transcript.Transcript;
 import Enums.InstructorType;
 import IDs.InstructorID;
 import IDs.StudentID;
 import lecture.Lecture;
+import lecture.LectureSession;
+import lecture.Semester;
 import person.Advisor;
 import person.Student;
 
@@ -22,16 +26,25 @@ public class StudentTest {
 	public void test_student_default_conscructor() {
 		InstructorID testInstructorID = new InstructorID(1,2);
 		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
-		Calendar testDateOfEntry = null;
+		Calendar testDateOfEntry = new GregorianCalendar(2009,10,28);
 		List<Student> testListOfStudents = new ArrayList<Student>();
 		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
 		InstructorType testInstructorType = InstructorType.Assistant;
 		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
 		
 		StudentID testStudentID = new StudentID(1,2,3);
+		List<LectureSession> testListOfLectureSessions = new ArrayList<LectureSession>();
 		
+		List<Semester> testListOfSemester = new ArrayList<Semester>();
+		double testGano = 1;
+		int testTotalCreditsTaken = 2;
+		int testTotalCreditsCompleted = 3;
+		double testPoints = 4;
 		
-		Student testStudent = new Student("John","Green",testStudentID,null,null,null);
+		Transcript testTranscript = new Transcript(null, testListOfSemester, testGano, testTotalCreditsTaken, testTotalCreditsCompleted, testPoints);
+		Calendar testStudentDateOfEntry = new GregorianCalendar(2010,10,28);
+		
+		Student testStudent = new Student("John","Green",testStudentID,testListOfLectureSessions,testTranscript,testStudentDateOfEntry);
 		
 		assertEquals(testAdvisor, testStudent.getAdvisor());
 		assertEquals(null, testStudent.getDebt());
