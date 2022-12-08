7fb32a68cf5415c8e476753fbcd0d91c95d5e63a
maknoli <serdar01alsan@outlook.com>
2022-12-08 16:41:47 +0300

    Added Unit Test To Advisor Class

--- src/test/unit/AdvisorTest.java
+++ src/test/unit/AdvisorTest.java
@@ -1,5 +1,36 @@
 package test.unit;
 
-public class AdvisorTest {
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.junit.Test;
+
+import Debt_LRA_Transcript.LectureRegistrationApplication;
+import Enums.InstructorType;
+import IDs.InstructorID;
+import lecture.Lecture;
+import person.Advisor;
+import person.Student;
+
+public class AdvisorTest {
+	@Test
+	public void test_advisor_default_conscructor() {
+		
+		InstructorID testInstructorID = new InstructorID(1,2);
+		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
+		Calendar testDateOfEntry = null;
+		List<Student> testListOfStudents = new ArrayList<Student>();
+		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
+		InstructorType testInstructorType = InstructorType.Assistant;
+		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
+	
+		assertEquals(testListOfStudents, testAdvisor.getListOfStudents());
+		assertEquals(testListOfApplications, testAdvisor.getListOfApplications());
+		
+	
+	
+	}
 }
