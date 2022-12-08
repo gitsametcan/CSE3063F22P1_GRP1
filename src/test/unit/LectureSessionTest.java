d02baeebade8ca6c0a03f5923a6255d7e141b97e
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2022-12-08 16:38:57 +0300

    Progress on Unit Test

--- src/test/unit/LectureSessionTest.java
+++ src/test/unit/LectureSessionTest.java
@@ -4,10 +4,26 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import org.junit.Test;
+
 import IDs.SessionID;
+import lecture.LectureSession;
+import Enums.SessionType;
 
 public class LectureSessionTest {
-	SessionID testID = new SessionID(000);
-	LectureSession testLectureSession = new LectureSession( );
+	
+	@Test
+	public void test_LectureSession_default_constructor() {
+	
+		SessionID testID = new SessionID(000);
+		
+		LectureSession testLectureSession = new LectureSession(testID, null, null,
+				SessionType.Application,null,null);
+		
+		assertEquals(000, testLectureSession.getSessionID());
+		
+		assertEquals(SessionType.Application, testLectureSession.getSessionType());
+		
+	}
 
 }
--- src/test/unit/LectureTest.java
+++ src/test/unit/LectureTest.java
@@ -12,15 +12,24 @@ import lecture.LectureSession;
 
 public class LectureTest {
 	
-	 @Test
-	 public void test_Lecture_default_constructor() {
-		 LectureID testID = new LectureID("000");
-		 Lecture testLecture = new Lecture(testID,"testName","FTE", 3, List<LectureSession> sessions,
-					Lecture prerequisite, 5);
-		 
-		 testLecture.setCredit("try_1");
-		 assertEquals("try_1", testLecture.getCredit())
-		 
-	 }
+	@Test
+	public void test_Lecture_default_constructor() {
+		LectureID testID = new LectureID("000");
+		Lecture testLecture = new Lecture(testID,"testName", LectureType.FTE, 3, null, null, 5);
+		
+		testLecture.setCredit(0);
+		assertEquals(0, testLecture.getCredit());
+		
+		testLecture.setLectureType(LectureType.NTE);
+		assertEquals(LectureType.NTE, testLecture.getLectureType());
+		
+		testLecture.setName("testString");
+		assertEquals("testString", testLecture.getName());
+		
+		testLecture.setQuota(23);
+		assertEquals(23, testLecture.getName());
+		
+		testLecture.addLectureSession(null);
+	}
 
 }
