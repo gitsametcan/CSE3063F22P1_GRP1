18216f3d0e99501a805c685424fac75d7db81a47
maknoli <serdar01alsan@outlook.com>
2022-12-11 00:44:22 +0300

    A little progress on Student Unit Test

--- src/test/unit/StudentTest.java
+++ src/test/unit/StudentTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import Debt_LRA_Transcript.Debt;
 import Debt_LRA_Transcript.LectureRegistrationApplication;
 import Debt_LRA_Transcript.Transcript;
 import Enums.InstructorType;
@@ -23,7 +24,7 @@ import person.Student;
 public class StudentTest {
 	
 	@Test
-	public void test_student_default_conscructor() {
+	public void test_student_advisor() {
 		InstructorID testInstructorID = new InstructorID(1,2);
 		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
 		Calendar testDateOfEntry = new GregorianCalendar(2009,10,28);
@@ -40,6 +41,7 @@ public class StudentTest {
 		int testTotalCreditsTaken = 2;
 		int testTotalCreditsCompleted = 3;
 		double testPoints = 4;
+		Debt testDebt = new Debt(12, null);
 		
 		Transcript testTranscript = new Transcript(null, testListOfSemester, testGano, testTotalCreditsTaken, testTotalCreditsCompleted, testPoints);
 		Calendar testStudentDateOfEntry = new GregorianCalendar(2010,10,28);
@@ -47,11 +49,167 @@ public class StudentTest {
 		Student testStudent = new Student("John","Green",testStudentID,testListOfLectureSessions,testTranscript,testStudentDateOfEntry);
 		
 		assertEquals(testAdvisor, testStudent.getAdvisor());
-		assertEquals(null, testStudent.getDebt());
+	}
+	@Test
+	public void test_student_debt() {
+		InstructorID testInstructorID = new InstructorID(1,2);
+		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
+		Calendar testDateOfEntry = new GregorianCalendar(2009,10,28);
+		List<Student> testListOfStudents = new ArrayList<Student>();
+		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
+		InstructorType testInstructorType = InstructorType.Assistant;
+		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
+		
+		StudentID testStudentID = new StudentID(1,2,3);
+		List<LectureSession> testListOfLectureSessions = new ArrayList<LectureSession>();
+		
+		List<Semester> testListOfSemester = new ArrayList<Semester>();
+		double testGano = 1;
+		int testTotalCreditsTaken = 2;
+		int testTotalCreditsCompleted = 3;
+		double testPoints = 4;
+		Debt testDebt = new Debt(12, null);
+		
+		Transcript testTranscript = new Transcript(null, testListOfSemester, testGano, testTotalCreditsTaken, testTotalCreditsCompleted, testPoints);
+		Calendar testStudentDateOfEntry = new GregorianCalendar(2010,10,28);
+		
+		Student testStudent = new Student("John","Green",testStudentID,testListOfLectureSessions,testTranscript,testStudentDateOfEntry);
+		
+		assertEquals(testDebt, testStudent.getDebt());
+	}
+	@Test
+	public void test_student_registiration_application() {
+		InstructorID testInstructorID = new InstructorID(1,2);
+		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
+		Calendar testDateOfEntry = new GregorianCalendar(2009,10,28);
+		List<Student> testListOfStudents = new ArrayList<Student>();
+		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
+		InstructorType testInstructorType = InstructorType.Assistant;
+		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
+		
+		StudentID testStudentID = new StudentID(1,2,3);
+		List<LectureSession> testListOfLectureSessions = new ArrayList<LectureSession>();
+		
+		List<Semester> testListOfSemester = new ArrayList<Semester>();
+		double testGano = 1;
+		int testTotalCreditsTaken = 2;
+		int testTotalCreditsCompleted = 3;
+		double testPoints = 4;
+		Debt testDebt = new Debt(12, null);
+		
+		Transcript testTranscript = new Transcript(null, testListOfSemester, testGano, testTotalCreditsTaken, testTotalCreditsCompleted, testPoints);
+		Calendar testStudentDateOfEntry = new GregorianCalendar(2010,10,28);
+		
+		Student testStudent = new Student("John","Green",testStudentID,testListOfLectureSessions,testTranscript,testStudentDateOfEntry);
+		
 		assertEquals(null, testStudent.getRegistirationApplication());
+	}
+	@Test
+	public void test_student_id() {
+		InstructorID testInstructorID = new InstructorID(1,2);
+		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
+		Calendar testDateOfEntry = new GregorianCalendar(2009,10,28);
+		List<Student> testListOfStudents = new ArrayList<Student>();
+		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
+		InstructorType testInstructorType = InstructorType.Assistant;
+		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
+		
+		StudentID testStudentID = new StudentID(1,2,3);
+		List<LectureSession> testListOfLectureSessions = new ArrayList<LectureSession>();
+		
+		List<Semester> testListOfSemester = new ArrayList<Semester>();
+		double testGano = 1;
+		int testTotalCreditsTaken = 2;
+		int testTotalCreditsCompleted = 3;
+		double testPoints = 4;
+		Debt testDebt = new Debt(12, null);
+		
+		Transcript testTranscript = new Transcript(null, testListOfSemester, testGano, testTotalCreditsTaken, testTotalCreditsCompleted, testPoints);
+		Calendar testStudentDateOfEntry = new GregorianCalendar(2010,10,28);
+		
+		Student testStudent = new Student("John","Green",testStudentID,testListOfLectureSessions,testTranscript,testStudentDateOfEntry);
+		
 		assertEquals("123", testStudent.getId());
-		assertEquals(null, testStudent.getListOfLectureSessions());
-		assertEquals(null, testStudent.getTranscript());
-		assertEquals(null, testStudent.getDateOfEntry());
+	}
+	@Test
+	public void test_student_list_of_lecture_sessions() {
+		InstructorID testInstructorID = new InstructorID(1,2);
+		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
+		Calendar testDateOfEntry = new GregorianCalendar(2009,10,28);
+		List<Student> testListOfStudents = new ArrayList<Student>();
+		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
+		InstructorType testInstructorType = InstructorType.Assistant;
+		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
+		
+		StudentID testStudentID = new StudentID(1,2,3);
+		List<LectureSession> testListOfLectureSessions = new ArrayList<LectureSession>();
+		
+		List<Semester> testListOfSemester = new ArrayList<Semester>();
+		double testGano = 1;
+		int testTotalCreditsTaken = 2;
+		int testTotalCreditsCompleted = 3;
+		double testPoints = 4;
+		Debt testDebt = new Debt(12, null);
+		
+		Transcript testTranscript = new Transcript(null, testListOfSemester, testGano, testTotalCreditsTaken, testTotalCreditsCompleted, testPoints);
+		Calendar testStudentDateOfEntry = new GregorianCalendar(2010,10,28);
+		
+		Student testStudent = new Student("John","Green",testStudentID,testListOfLectureSessions,testTranscript,testStudentDateOfEntry);
+		
+		assertEquals(testListOfLectureSessions, testStudent.getListOfLectureSessions());
+	}
+	@Test
+	public void test_student_transcript() {
+		InstructorID testInstructorID = new InstructorID(1,2);
+		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
+		Calendar testDateOfEntry = new GregorianCalendar(2009,10,28);
+		List<Student> testListOfStudents = new ArrayList<Student>();
+		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
+		InstructorType testInstructorType = InstructorType.Assistant;
+		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
+		
+		StudentID testStudentID = new StudentID(1,2,3);
+		List<LectureSession> testListOfLectureSessions = new ArrayList<LectureSession>();
+		
+		List<Semester> testListOfSemester = new ArrayList<Semester>();
+		double testGano = 1;
+		int testTotalCreditsTaken = 2;
+		int testTotalCreditsCompleted = 3;
+		double testPoints = 4;
+		Debt testDebt = new Debt(12, null);
+		
+		Transcript testTranscript = new Transcript(null, testListOfSemester, testGano, testTotalCreditsTaken, testTotalCreditsCompleted, testPoints);
+		Calendar testStudentDateOfEntry = new GregorianCalendar(2010,10,28);
+		
+		Student testStudent = new Student("John","Green",testStudentID,testListOfLectureSessions,testTranscript,testStudentDateOfEntry);
+		
+		assertEquals(testTranscript, testStudent.getTranscript());
+	}
+	@Test
+	public void test_student_date_of_entry() {
+		InstructorID testInstructorID = new InstructorID(1,2);
+		List<Lecture> testListOfLectures = new ArrayList<Lecture>();
+		Calendar testDateOfEntry = new GregorianCalendar(2009,10,28);
+		List<Student> testListOfStudents = new ArrayList<Student>();
+		List<LectureRegistrationApplication> testListOfApplications = new ArrayList<LectureRegistrationApplication>();
+		InstructorType testInstructorType = InstructorType.Assistant;
+		Advisor testAdvisor = new Advisor("A","B",testInstructorID,testListOfLectures,testDateOfEntry,testListOfStudents,testListOfApplications,testInstructorType);
+		
+		StudentID testStudentID = new StudentID(1,2,3);
+		List<LectureSession> testListOfLectureSessions = new ArrayList<LectureSession>();
+		
+		List<Semester> testListOfSemester = new ArrayList<Semester>();
+		double testGano = 1;
+		int testTotalCreditsTaken = 2;
+		int testTotalCreditsCompleted = 3;
+		double testPoints = 4;
+		Debt testDebt = new Debt(12, null);
+		
+		Transcript testTranscript = new Transcript(null, testListOfSemester, testGano, testTotalCreditsTaken, testTotalCreditsCompleted, testPoints);
+		Calendar testStudentDateOfEntry = new GregorianCalendar(2010,10,28);
+		
+		Student testStudent = new Student("John","Green",testStudentID,testListOfLectureSessions,testTranscript,testStudentDateOfEntry);
+		
+		assertEquals(testStudentDateOfEntry, testStudent.getDateOfEntry());
 	}
 }
