615d43c86a9a065f71c41afa75bc7c33c7e8b0b4
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2022-12-13 15:18:49 +0300

    Simulation Package Created

---
+++ src/simulation/StudentGenerator.java
@@ -0,0 +1,10 @@
+package simulation;
+
+public class StudentGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/simulation/TranscriptGenerator.java
@@ -0,0 +1,10 @@
+package simulation;
+
+public class TranscriptGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
