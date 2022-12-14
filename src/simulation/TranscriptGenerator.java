62c7eb2ffdd8efedba3b6216482c6d6f95aaae7d
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2022-12-14 14:17:23 +0300

    Update Semester Class

--- src/simulation/TranscriptGenerator.java
+++ src/simulation/TranscriptGenerator.java
@@ -10,17 +10,7 @@ public class TranscriptGenerator {
 
 	private Student student;
 	private List<Semester> listOfSemester;
-
-	private void setGano(List<Semester> listOfSemester) {
-		double tempGano = 0;
-		int numberOfSemester = 0;
-		for (int i = 0; i < listOfSemester.size(); i++) {
-			double yano = listOfSemester.get(i).getYano();
-			tempGano = tempGano + yano;
-			numberOfSemester++;
-		}
-
-	}
+	
 
 	Transcript transcript = new Transcript(student, listOfSemester);
 }
