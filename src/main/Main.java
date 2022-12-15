0850ac9f3d32dab83ee3ad6bbc1fdeff4fb60ae7
Kadir Berk Yağar <ykadirberk2@gmail.com>
2022-12-15 10:50:37 +0300

    Log4j api

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,17 @@ package main;
 
 import java.io.FileNotFoundException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import System.RegistrationSystem;
 
 public class Main {
+	
+	private static final Logger LOG = LogManager.getLogger(Main.class);
 
 	public static void main(String[] args) throws FileNotFoundException {
+		LOG.info("Appending string: {}.", "Hello, World");
 
 		RegistrationSystem registrationSystem = new RegistrationSystem();
 
