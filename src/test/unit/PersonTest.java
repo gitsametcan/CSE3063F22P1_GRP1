7f9b1637f7edf3c6496381b5e52c758add65bc7c
maknoli <serdar01alsan@outlook.com>
2022-12-16 13:53:22 +0300

    Updating persontest class

--- src/test/unit/PersonTest.java
+++ src/test/unit/PersonTest.java
@@ -4,20 +4,20 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import person.Person;
+import person.Student;
 
 public class PersonTest {
 
 	@Test
 	public void test_person_first_name() {
-		Person testPersonClass = new Person("John","Green");
+		Student testPersonClass = new Student("John","Green",null,null,null,null);
 		
 		testPersonClass.setFirstName("John");
 		assertEquals("John", testPersonClass.getFirstName());
 	}
 	@Test
 	public void test_person_last_name() {
-		Person testPersonClass = new Person("John","Green");
+		Student testPersonClass = new Student("John","Green",null,null,null,null);
 		
 		testPersonClass.setLastName("Green");
 		assertEquals("Green", testPersonClass.getLastName());
@@ -25,7 +25,7 @@ public class PersonTest {
 	}
 	@Test
 	public void test_person_full_name() {
-		Person testPersonClass = new Person("John","Green");
+		Student testPersonClass = new Student("John","Green",null,null,null,null);
 		
 		testPersonClass.setFirstName("John");
 		
