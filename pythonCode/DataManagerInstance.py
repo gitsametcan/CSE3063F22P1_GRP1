071118d198fd58570632af62d1da97200f50c3b2
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-01-05 11:59:21 +0300

    added search methods for Lecture, Advisor and Student

--- pythonCode/DataManagerInstance.py
+++ pythonCode/DataManagerInstance.py
@@ -24,7 +24,7 @@ class DataManagerInstance():
             if s.getID() == key and filterType is FilterType.ID and isinstance(s, Student):
                 student : Student = s
                 return student
-            elif s.getName() == key and filterType is FilterType.Name and isinstance(s, Student):
+            elif s.getFullName() == key and filterType is FilterType.Name and isinstance(s, Student):
                 student : Student = s
                 return student
         return None
@@ -35,10 +35,44 @@ class DataManagerInstance():
             if a.getID() == key and filterType is FilterType.ID and isinstance(a, Advisor):
                 advisor : Advisor = a
                 return advisor
-            elif a.getName() == key and filterType is FilterType.Name and isinstance(a, Advisor):
+            elif a.getFullName() == key and filterType is FilterType.Name and isinstance(a, Advisor):
                 advisor : Advisor = a
                 return advisor
         return None
+
+    def searchLectures(self, key: str, filterType: FilterType):
+        result = list()
+        for l in self.__listOfLectures:
+            if key in l.getID() and filterType is FilterType.ID:
+                result.append(l)
+            if key in l.getName() and filterType is FilterType.Name:
+                result.appent(l)
+        return result
+
+    def searchStudents(self, key: str, filterType: FilterType):
+        from Student import Student
+        result = list()
+        for s in self.__listOfPeople:
+            if key in s.getID() and filterType is FilterType.ID and isinstance(s, Student):
+                student : Student = s
+                result.append(student)
+            if key in s.getFullName() and filterType is FilterType.ID and isinstance(s, Student):
+                student : Student = s
+                result.append(student)
+        return result
+        
+    def searchAdvisors(self, key: str, filterType: FilterType):
+        from Advisor import Advisor
+        result = list()
+        for a in self.__listOfPeople:
+            if key in a.getID() and filterType is FilterType.ID and isinstance(a, Advisor):
+                advisor : Advisor = a
+                result.append(advisor)
+            if key in s.getFullName() and filterType is FilterType.ID and isinstance(a, Advisor):
+                advisor : Advisor = a
+                result.append(advisor)
+        return result
+
 
     def hello(self):
        print("hello")
